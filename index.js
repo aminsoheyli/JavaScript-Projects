366b4cb472e37637b54be0cfaebca21a75dfb4e3
Seyed Mohammad Amin Soheyli <aminsoheyli98@gmail.com>
2020-09-22 11:32:29 +0330

    rebase

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Listening on port: ' + port + '...'));
