7401da627221702407766374b8ec4fa0266f8d82
Seyed Mohammad Amin Soheyli <aminsoheyli98@gmail.com>
2020-09-23 19:49:26 +0330

    add rentals api with transaction

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const app = express();
 
 mongoose.connect('mongodb://localhost/vidly')
@@ -12,7 +13,8 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
-app.use('/api/movies', movies)
+app.use('/api/movies', movies);
+app.use('/api/rentals',rentals);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log('Listening on port: ' + port + '...'));
