c1b61c959817ab316a0776c122ea9ca486dc5717
Seyed Mohammad Amin Soheyli <aminsoheyli98@gmail.com>
2020-10-25 01:48:48 +0330

    Refactor the store using redux-dev-tools

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,42 +1,28 @@
-// Action types
-const BUG_ADDED = "bugAdded";
-const BUG_REMOVED = "bugRemoved";
-const BUG_RESOLVED = "bugResolved";
+import {createSlice} from "@reduxjs/toolkit";
 
-// Action creators
-export const bugAdded = description => ({
-    type: BUG_ADDED,
-    payload:{
-        description
+let lastId = 0;
+
+const slice = createSlice({
+    name: 'bugs',
+    initialState: [],
+    reducers: {
+        bugAdded: (bugs, action) => {
+            bugs.push({
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false,
+            });
+        },
+        bugResolved: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id);
+            bugs[index].resolved = true;
+        },
+        bugRemoved: (bugs, action) => {
+            const index = bugs.findIndex(bug => bug.id === action.payload.id);
+            bugs.splice(index, 1)
+        }
     }
-});
+})
 
-export const bugResolved = id => ({
-    type: BUG_RESOLVED,
-    payload:{
-        id
-    }
-});
-
-// Reducer
-let lastId = 0
-
-export default function reducer(state = [], action) {
-    switch (action.type) {
-        case BUG_ADDED:
-            return [
-                ...state,
-                {
-                    id: ++lastId,
-                    description: action.payload.description,
-                    resolved: false,
-                }
-            ];
-        case BUG_REMOVED:
-            return state.filter(bug => bug.id !== action.payload.id);
-        case BUG_RESOLVED:
-            return state.map(bug => bug.id === action.payload.id ? {...bug, resolved: true} : bug);
-        default:
-            return state
-    }
-}
+export const {bugAdded, bugRemoved, bugResolved} = slice.actions;
+export default slice.reducer;
