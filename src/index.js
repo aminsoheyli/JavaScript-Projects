e9c73082a7f54902cae471fa39a020b2ba818c5d
Seyed Mohammad Amin Soheyli <aminsoheyli98@gmail.com>
2020-10-25 13:55:08 +0330

    Add logger and toast middlewares

--- src/index.js
+++ src/index.js
@@ -6,19 +6,31 @@ import {userAdded} from "./store/users";
 
 const store = configureStore();
 
+store.dispatch((dispatch, getState)=>{
+    dispatch({type: 'bugsReceived', bugs:[1,2,3]})
+    console.log(getState())
+});
+
+store.dispatch({
+    type: 'error',
+    payload: {
+        message: "Error occurred"
+    }
+})
+
 const unsubscribe = store.subscribe(() => {
     console.log("Store changed!", store.getState());
 });
 
 store.dispatch(userAdded({name: "User 1"}));
-store.dispatch(userAdded({name: "User 2"}));
-store.dispatch(projectAdded({name: 'Project 1'}));
-store.dispatch(bugAdded({description: " Bug 1"}));
-store.dispatch(bugAdded({description: " Bug 2"}));
-store.dispatch(bugAdded({description: " Bug 3"}));
-store.dispatch(bugAssignedToUser({bugId:1,userId:1}));
-store.dispatch(bugResolved({id: 3}));
-store.dispatch(bugRemoved({id: 2}));
+// store.dispatch(userAdded({name: "User 2"}));
+// store.dispatch(projectAdded({name: 'Project 1'}));
+// store.dispatch(bugAdded({description: " Bug 1"}));
+// store.dispatch(bugAdded({description: " Bug 2"}));
+// store.dispatch(bugAdded({description: " Bug 3"}));
+// store.dispatch(bugAssignedToUser({bugId:1,userId:1}));
+// store.dispatch(bugResolved({id: 3}));
+// store.dispatch(bugRemoved({id: 2}));
 
 const x = getUnresolvedBugs(store.getState());
 const y = getUnresolvedBugs(store.getState());
